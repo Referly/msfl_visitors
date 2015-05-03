af35450a59048dc4026741ddda8bf6720c72b31b
Courtland Caldwell <courtland@mattermark.com>
2015-05-03 16:45:38 -0700

    Added the grouping namespace of nodes

---
+++ lib/msfl/nodes/grouping/close.rb
@@ -0,0 +1,9 @@
+require_relative '../constant_value'
+module MSFL
+  module Nodes
+    module Grouping
+      class Close < ConstantValue
+      end
+    end
+  end
+end
---
+++ lib/msfl/nodes/grouping/grouping.rb
@@ -0,0 +1,23 @@
+require_relative 'close'
+require_relative 'open'
+module MSFL
+  module Nodes
+    module Grouping
+      class Grouping
+
+        attr_accessor :contents
+
+        def accept(visitor)
+          Open.new.accept visitor
+          contents.accept visitor
+          Close.new.accept visitor
+        end
+
+        # @param node [MSFL::Nodes::Base] the node that the grouping surrounds
+        def initialize(node)
+          self.contents = node
+        end
+      end
+    end
+  end
+end
