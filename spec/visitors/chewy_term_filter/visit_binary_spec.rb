ed4596536cce1d0c65959be98906eed482ce5365
Courtland Caldwell <courtland@mattermark.com>
2015-05-03 16:47:28 -0700

    Updated spec to match latest convention for testing visit_ methods

--- spec/visitors/chewy_term_filter/visit_binary_spec.rb
+++ spec/visitors/chewy_term_filter/visit_binary_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe MSFL::Visitors::ChewyTermFilter do
 
-  let(:collector) { String.new }
+  subject { node.accept visitor }
 
   let(:node) { fail ArgumentError, "You must define the node variable in each scope." }
 
-  let(:test_instance) { described_class.new collector }
+  let(:visitor) { described_class.new collector }
+
+  let(:collector) { String.new }
 
   let(:left) { MSFL::Nodes::Word.new "lhs" }
 
   let(:right) { MSFL::Nodes::Word.new "rhs" }
 
   describe "#visit_MSFL_Nodes_And" do
-
-    subject { node.accept test_instance }
 
     let(:node) { MSFL::Nodes::And.new left, right }
 
