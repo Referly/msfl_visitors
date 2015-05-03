c1454c6358d944772a9a9a7c1791e53ba251d6bd
Courtland Caldwell <courtland@mattermark.com>
2015-05-03 16:42:57 -0700

    Completed updates to visit_comparison_spec

--- spec/visitors/chewy_term_filter/visit_comparison_spec.rb
+++ spec/visitors/chewy_term_filter/visit_comparison_spec.rb
@@ -2,68 +2,60 @@ require 'spec_helper'
 
 describe MSFL::Visitors::ChewyTermFilter do
 
-  let(:collector) { String.new }
+  subject { node.accept visitor }
 
   let(:node) { fail ArgumentError, "You must define the node variable in each scope." }
 
-  let(:test_instance) { described_class.new }
+  let(:visitor) { described_class.new collector }
+
+  let(:collector) { String.new }
 
   let(:left) { MSFL::Nodes::Word.new "lhs" }
 
   let(:right) { MSFL::Nodes::Word.new "rhs" }
 
-  describe "#visit_MSFL_Nodes_Equal" do
-
-    subject { test_instance.visit_MSFL_Nodes_Equal node, collector }
+  describe "visiting an Equal node" do
 
     let(:node) { MSFL::Nodes::Equal.new left, right }
 
-    it "matches ( left == right )" do
-      expect(subject).to match "( lhs == rhs )"
+    it "matches left == right" do
+      expect(subject).to match "lhs == rhs"
     end
   end
 
-  describe "#visit_MSFL_Nodes_GreaterThan" do
-
-    subject { test_instance.visit_MSFL_Nodes_GreaterThan node, collector }
+  describe "visiting a GreaterThan node" do
 
     let(:node) { MSFL::Nodes::GreaterThan.new left, right }
 
-    it "matches ( left > right )" do
-      expect(subject).to match "( lhs > rhs )"
+    it "matches left > right" do
+      expect(subject).to match "lhs > rhs"
     end
   end
 
-  describe "#visit_MSFL_Nodes_GreaterThanEqual" do
-
-    subject { test_instance.visit_MSFL_Nodes_GreaterThanEqual node, collector }
+  describe "visiting a GreaterThanEqual node" do
 
     let(:node) { MSFL::Nodes::GreaterThanEqual.new left, right }
 
-    it "matches ( left >= right )" do
-      expect(subject).to match "( lhs >= rhs )"
+    it "matches left >= right" do
+      expect(subject).to match "lhs >= rhs"
     end
   end
 
-  describe "#visit_MSFL_Nodes_LessThan" do
-
-    subject { test_instance.visit_MSFL_Nodes_LessThan node, collector }
+  describe "visiting a LessThan node" do
 
     let(:node) { MSFL::Nodes::LessThan.new left, right }
 
-    it "matches ( left < right )" do
-      expect(subject).to match "( lhs < rhs )"
+    it "matches left < right" do
+      expect(subject).to match "lhs < rhs"
     end
   end
 
-  describe "#visit_MSFL_Nodes_LessThanEqual" do
-
-    subject { test_instance.visit_MSFL_Nodes_LessThanEqual node, collector }
+  describe "visiting a LessThanEqual node" do
 
     let(:node) { MSFL::Nodes::LessThanEqual.new left, right }
 
-    it "matches ( left <= right )" do
-      expect(subject).to match "( lhs <= rhs )"
+    it "matches left <= right" do
+      expect(subject).to match "lhs <= rhs"
     end
   end
 end
