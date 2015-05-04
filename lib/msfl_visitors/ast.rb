d2586fe424e81a55adc197ac1ebe70dfa83df2a8
Courtland Caldwell <courtland@mattermark.com>
2015-05-04 12:30:15 -0700

    Added accept method to AST which will serve as the generally accepted interface for starting a visitor traversal of the tree.

--- lib/msfl_visitors/ast.rb
+++ lib/msfl_visitors/ast.rb
@@ -6,5 +6,10 @@ module MSFLVisitors
     def initialize(msfl)
       self.root = MSFLVisitors::Parsers::MSFLParser.parse msfl
     end
+
+    # Use this method to walk the AST with a particular visitor
+    def accept(visitor)
+      root.accept visitor
+    end
   end
 end
