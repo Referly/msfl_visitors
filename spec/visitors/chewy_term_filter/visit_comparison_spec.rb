f4bda8732f72b2cbc6373cb8d334566860b34b84
Matt Van Horn <matt@mattermark.com>
2015-05-04 12:48:15 -0700

    Language/semantics change

--- spec/visitors/chewy_term_filter/visit_comparison_spec.rb
+++ spec/visitors/chewy_term_filter/visit_comparison_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe MSFLVisitors::Visitors::ChewyTermFilter do
 
-  subject { node.accept visitor }
-
   let(:node) { fail ArgumentError, "You must define the node variable in each scope." }
 
   let(:visitor) { described_class.new collector }
@@ -14,48 +12,50 @@ describe MSFLVisitors::Visitors::ChewyTermFilter do
 
   let(:right) { MSFLVisitors::Nodes::Word.new "rhs" }
 
-  describe "visiting an Equal node" do
+  subject(:result) { node.accept visitor }
+
+  describe "the result of visiting an Equal node" do
 
     let(:node) { MSFLVisitors::Nodes::Equal.new left, right }
 
-    it "matches left == right" do
-      expect(subject).to match "lhs == rhs"
+    it "is: 'left == right'" do
+      expect(result).to eq "lhs == rhs"
     end
   end
 
-  describe "visiting a GreaterThan node" do
+  describe "the result of visiting a GreaterThan node" do
 
     let(:node) { MSFLVisitors::Nodes::GreaterThan.new left, right }
 
-    it "matches left > right" do
-      expect(subject).to match "lhs > rhs"
+    it "is: 'left > right'" do
+      expect(result).to eq "lhs > rhs"
     end
   end
 
-  describe "visiting a GreaterThanEqual node" do
+  describe "the result of visiting a GreaterThanEqual node" do
 
     let(:node) { MSFLVisitors::Nodes::GreaterThanEqual.new left, right }
 
-    it "matches left >= right" do
-      expect(subject).to match "lhs >= rhs"
+    it "is: 'left >= right'" do
+      expect(result).to eq "lhs >= rhs"
     end
   end
 
-  describe "visiting a LessThan node" do
+  describe "the result of visiting a LessThan node" do
 
     let(:node) { MSFLVisitors::Nodes::LessThan.new left, right }
 
-    it "matches left < right" do
-      expect(subject).to match "lhs < rhs"
+    it "is: 'left < right'" do
+      expect(result).to eq "lhs < rhs"
     end
   end
 
-  describe "visiting a LessThanEqual node" do
+  describe "the result of visiting a LessThanEqual node" do
 
     let(:node) { MSFLVisitors::Nodes::LessThanEqual.new left, right }
 
-    it "matches left <= right" do
-      expect(subject).to match "lhs <= rhs"
+    it "is: 'left <= right'" do
+      expect(result).to eq "lhs <= rhs"
     end
   end
 end
