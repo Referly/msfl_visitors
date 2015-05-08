b4a9ebb60251c4b5a543378aa64e95c9548cadd3
Courtland Caldwell <courtland@mattermark.com>
2015-05-08 10:05:41 -0700

    Reduce spec complexity

--- spec/parsers/msfl_parser_spec.rb
+++ spec/parsers/msfl_parser_spec.rb
@@ -24,13 +24,7 @@ describe MSFLVisitors::Parsers::MSFLParser do
 
     context "when parsing a filter" do
 
-      subject { -> (filter) { described_class.new.parse filter } }
-
-      let(:implicit_equality_filter) { { value: 1000 } }
-
-      let(:explicit_gte_filter) { { value: { gte: 1000 } } }
-
-      let(:containment_filter) { { value: { in: set_of_values } } }
+      subject { described_class.new.parse(filter) }
 
       let(:set_of_values) { MSFL::Types::Set.new [50, 250, 20000] }
 
@@ -38,44 +32,62 @@ describe MSFLVisitors::Parsers::MSFLParser do
 
       let(:set_node) { MSFLVisitors::Nodes::Set::Set.new set_of_nodes }
 
-      it "handles implicit equality comparisons" do
-        expect(subject.call(implicit_equality_filter)).to eq expected_node.call(MSFLVisitors::Nodes::Equal.new(left, right))
+      describe "parsing implicit equality" do
+
+        let(:filter) { { value: 1000 } }
+
+        it "is the expected Equal node" do
+          expect(subject).to eq expected_node.call(MSFLVisitors::Nodes::Equal.new(left, right))
+        end
       end
 
-      it "handles explicit comparisons" do
-        comparison_node = MSFLVisitors::Nodes::GreaterThanEqual.new left, right
-        expect(subject.call(explicit_gte_filter)).to eq expected_node.call(comparison_node)
+      describe "parsing explict comparisons" do
+
+        describe "parsing a gte filter" do
+
+          let(:filter) { { value: { gte: 1000 } } }
+
+          it "is the expected GreaterThanEqual node" do
+            comparison_node = MSFLVisitors::Nodes::GreaterThanEqual.new left, right
+            expect(subject).to eq expected_node.call(comparison_node)
+          end
+        end
       end
 
-      # { value: { in: [50, 250, 20000] } }
-      #
-      #  => Nodes::Containment.new(Nodes::Field.new(:value),
-      #                            Nodes::Set::Set.new([
-      #                               Nodes::Number.new(50),
-      #                               Nodes::Number.new(250),
-      #                               Nodes::Number.new(20000)]))
-      it "handles containments" do
-        containment_node = MSFLVisitors::Nodes::Containment.new left, set_node
-        expect(subject.call(containment_filter)).to eq expected_node.call(containment_node)
+      describe "parsing containment" do
+
+        let(:filter) { { value: { in: set_of_values } } }
+
+        # { value: { in: [50, 250, 20000] } }
+        #
+        #  => Nodes::Containment.new(Nodes::Field.new(:value),
+        #                            Nodes::Set::Set.new([
+        #                               Nodes::Number.new(50),
+        #                               Nodes::Number.new(250),
+        #                               Nodes::Number.new(20000)]))
+        it "handles containments" do
+          containment_node = MSFLVisitors::Nodes::Containment.new left, set_node
+          expect(subject).to eq expected_node.call(containment_node)
+        end
       end
 
       describe "parsing an and filter" do
 
-        let(:and_filter) { { and: set_of_values } }
+        let(:filter) { { and: set_of_values } }
 
         let(:and_node) { MSFLVisitors::Nodes::And.new set_node }
 
         it "parses the filter" do
-          expect(subject.call(and_filter)).to eq expected_node.call(and_node)
+          expect(subject).to eq expected_node.call(and_node)
         end
       end
 
       context "when the filter contains an unsupported type" do
 
-        let(:bad_filter) { { foo: Object.new } }
+        let(:filter) { { foo: Object.new } }
 
         it "raises an ArgumentError" do
-          expect { subject.call(bad_filter) }.to raise_error ArgumentError
+          expect { subject }.to raise_error ArgumentError
         end
       end
     end
